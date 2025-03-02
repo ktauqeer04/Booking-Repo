e92fc8186a4c55fc7cb0ae17d0019fe42ee12a9d
Mohammad Tauqeer Kondkari <mohammedtauqeer4oct@gmail.com>
2025-03-02 23:37:42 +0530

    transactions and microservices communication

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -6,7 +6,9 @@ const getFlight = async (req, res) => {
 
     try {
         const { flightId, noOfSeats } = req.body;
-        const flightDetails = createBooking({ flightId, noOfSeats });
+        const flightDetails = await createBooking({ flightId, noOfSeats });
+        console.log(flightDetails);
+        
         SuccessResponse.data = flightDetails;
         return res.status(StatusCodes.OK).json(SuccessResponse);
     } catch (error) {
