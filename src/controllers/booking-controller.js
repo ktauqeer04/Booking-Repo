bf0a7fbabcd7b3142a37535c4d173e14f2252b19
Mohammad Tauqeer Kondkari <mohammedtauqeer4oct@gmail.com>
2025-03-03 16:47:54 +0530

    transactions in tryCatch

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -8,11 +8,11 @@ const getFlight = async (req, res) => {
         const { flightId, noOfSeats } = req.body;
         const flightDetails = await createBooking({ flightId, noOfSeats });
         console.log(flightDetails);
-        
         SuccessResponse.data = flightDetails;
         return res.status(StatusCodes.OK).json(SuccessResponse);
     } catch (error) {
         SuccessResponse.error = error;
+        console.log(error);
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(SuccessResponse);
     }
 
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,19 +1,26 @@
-const db = require('../models');
 const axios = require('axios');
+const db = require('../models');
 const { StatusCodes } = require('http-status-codes');
 const AppError = require('../utils/errors/app-error');
 
 const createBooking = async (data) => {
-    return new Promise((resolve, reject) => {
-        db.sequelize.transaction(async function bookingImplementation() {
-            const flight = await axios.get(`http://localhost:4000/api/v1/flights/${data.flightId}`);
-            const flightData = flight.data.data;
-            if(data.noOfSeats > flightData.totalSeats){
-                return reject(new AppError('Flight Seats Unavailable right Now', StatusCodes.BAD_REQUEST));
-            }
-            return resolve(flightData);
-        })
-    })
+
+    const transaction = await db.sequelize.transaction();
+
+    try {
+        
+        const flight = await axios.get(`http://localhost:4000/api/v1/flights/${data.flightId}`);
+        const flightData = flight.data.data;
+        if(data.noOfSeats > flightData.totalSeats){
+            throw new AppError('Flight Seats Unavailable right Now', StatusCodes.BAD_REQUEST);
+        }
+        await transaction.commit();
+        return flightData;
+
+    } catch (error) {
+        await transaction.rollback();
+        console.log(error);
+    }
 } 
 
 module.exports = {
